cfa45ee57596a12d74cec3d4af5f3f48e8d77c23
raphoucs <raphael.cohensalmon@gmail.com>
2021-01-25 21:53:13 +0100

    blackscholes : rajout de callbound & payoff

--- blackscholes.cpp
+++ blackscholes.cpp
@@ -1,10 +1,44 @@
 #include "blackscholes.h"
 
+pdefunc generateCallPayoff(double K) {
+    return [K](double x, double t)->double {
+        double res = exp(x) - K;
+        return (res > 0) ? res : 0;
+    };
+}
+
+
+pdefunc generateCallBound(double r, double K, double T) {
+    return [r, K, T](double x, double t)->double {
+        double res = exp(x) - K * exp(-r * (T - t));
+        return (res >= 0) ? res : 0;
+    };
+}
+
 
 pdefunc generateConstant(double c) {
 	return [c](double x, double t)->double {
 		return c;
 	};
+}
+
+vector<double> generateMesh(double start, double end, int size) {
+    double step = (end - start) / (size - 1);
+    vector<double> res(size);
+    for (int i = 0; i < size; ++i) {
+        res[i] = start + i * step;
+    }
+    return res;
+}
+
+vector<double> generateSpotMesh(double spot, double std, int n){
+    vector<double> res(2 * n + 1);
+    double step = 5 * std / n;
+    double spot_x = log(spot);
+    for (int i = 0; i < 2*n+1; ++i) {
+        res[i] = spot_x - 5 * std + i * step;
+    }
+    return res;
 }
 
 PDE generate_BS_PDE(pdefunc r, pdefunc sigma, vector<double> meshX, vector<double> meshT, PDEBounds bounds, double theta, bool constCoef, bool constBound) {
